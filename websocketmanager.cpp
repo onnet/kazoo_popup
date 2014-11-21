a2f6e3dce1cf587d469c54b8948b194f9701fa14
Alexey Lysenko <lysenkoalexmail@gmail.com>
2014-11-21 18:26:41 +0200

    convert websocket error to human-readable string

--- websocketmanager.cpp
+++ websocketmanager.cpp
@@ -32,6 +32,8 @@ enum CallDirection
     kCallDirectionOutbound
 };
 
+QString socketErrorToString(QAbstractSocket::SocketError error);
+
 WebSocketManager::WebSocketManager(QObject *parent) :
     QObject(parent)
 {
@@ -125,9 +127,9 @@ void WebSocketManager::retrieveAuthTokenFinished()
     QNetworkReply *reply = qobject_cast<QNetworkReply*>(sender());
     if (reply->error() != QNetworkReply::NoError)
     {
-        qDebug("NetworkReply error - func: %s, error: %d, request: %s",
+        qDebug("%s, NetworkReply error: %s, request: %s",
                Q_FUNC_INFO,
-               reply->error(),
+               reply->errorString().toLatin1().data(),
                reply->request().url().toString().toLatin1().data());
         reply->deleteLater();
         return;
@@ -171,9 +173,9 @@ void WebSocketManager::retrieveDevicesFinished()
     QNetworkReply *reply = qobject_cast<QNetworkReply*>(sender());
     if (reply->error() != QNetworkReply::NoError)
     {
-        qDebug("NetworkReply error - func: %s, error: %d, request: %s",
+        qDebug("%s, NetworkReply error: %s, request: %s",
                Q_FUNC_INFO,
-               reply->error(),
+               reply->errorString().toLatin1().data(),
                reply->request().url().toString().toLatin1().data());
         reply->deleteLater();
         return;
@@ -215,9 +217,9 @@ void WebSocketManager::retrieveWsAddressFinished()
     QNetworkReply *reply = qobject_cast<QNetworkReply*>(sender());
     if (reply->error() != QNetworkReply::NoError)
     {
-        qDebug("NetworkReply error - func: %s, error: %d, request: %s",
+        qDebug("%s, NetworkReply error: %s, request: %s",
                Q_FUNC_INFO,
-               reply->error(),
+               reply->errorString().toLatin1().data(),
                reply->request().url().toString().toLatin1().data());
         reply->deleteLater();
         return;
@@ -422,7 +424,7 @@ void WebSocketManager::handleConnectionError()
     if (m_webSocket != nullptr)
     {
         qDebug("WebSocket error: %s",
-               m_webSocket->errorString().toLatin1().data());
+               socketErrorToString(m_webSocket->error()).toLatin1().data());
     }
     stop();
     emit connectionError();
@@ -433,4 +435,59 @@ void WebSocketManager::checkPingTimeout()
     qint64 currentTimestamp = QDateTime::currentMSecsSinceEpoch();
     if (currentTimestamp - m_lastPing > kPermittedPingTimeout)
         handleConnectionError();
+}
+
+QString socketErrorToString(QAbstractSocket::SocketError error)
+{
+    switch (error) {
+    case QAbstractSocket::ConnectionRefusedError:
+        return "The connection was refused by the peer (or timed out)";
+    case QAbstractSocket::RemoteHostClosedError:
+        return "The remote host closed the connection";
+    case QAbstractSocket::HostNotFoundError:
+        return "The host address was not found";
+    case QAbstractSocket::SocketAccessError:
+        return "The socket operation failed because the application lacked the required privileges";
+    case QAbstractSocket::SocketResourceError:
+        return "The local system ran out of resources (e.g., too many sockets)";
+    case QAbstractSocket::SocketTimeoutError:
+        return "The socket operation timed out";
+    case QAbstractSocket::DatagramTooLargeError:
+        return "The datagram was larger than the operating system's limit (which can be as low as 8192 bytes)";
+    case QAbstractSocket::NetworkError:
+        return "An error occurred with the network (e.g., the network cable was accidentally plugged out)";
+    case QAbstractSocket::AddressInUseError:
+        return "The specified address is already in use and was set to be exclusive";
+    case QAbstractSocket::SocketAddressNotAvailableError:
+        return "The specified address does not belong to the host";
+    case QAbstractSocket::UnsupportedSocketOperationError:
+        return "The requested socket operation is not supported by the local operating system (e.g., lack of IPv6 support)";
+    case QAbstractSocket::ProxyAuthenticationRequiredError:
+    return "The socket is using a proxy, and the proxy requires authentication";
+    case QAbstractSocket::SslHandshakeFailedError:
+        return "The SSL/TLS handshake failed, so the connection was closed";
+    case QAbstractSocket::UnfinishedSocketOperationError:
+        return "The last operation attempted has not finished yet (still in progress in the background)";
+    case QAbstractSocket::ProxyConnectionRefusedError:
+        return "Could not contact the proxy server because the connection to that server was denied";
+    case QAbstractSocket::ProxyConnectionClosedError:
+        return "The connection to the proxy server was closed unexpectedly (before the connection to the final peer was established)";
+    case QAbstractSocket::ProxyConnectionTimeoutError:
+        return "The connection to the proxy server timed out or the proxy server stopped responding in the authentication phase";
+    case QAbstractSocket::ProxyNotFoundError:
+        return "The proxy address was not found";
+    case QAbstractSocket::ProxyProtocolError:
+        return "The connection negotiation with the proxy server failed, because the response from the proxy server could not be understood";
+    case QAbstractSocket::OperationError:
+        return "An operation was attempted while the socket was in a state that did not permit it";
+    case QAbstractSocket::SslInternalError:
+        return "The SSL library being used reported an internal error. This is probably the result of a bad installation or misconfiguration of the library";
+    case QAbstractSocket::SslInvalidUserDataError:
+        return "Invalid data (certificate, key, cypher, etc.) was provided and its use resulted in an error in the SSL library";
+    case QAbstractSocket::TemporaryError:
+        return "A temporary error occurred (e.g., operation would block and socket is non-blocking)";
+    case QAbstractSocket::UnknownSocketError:
+    default:
+        return "An unidentified websocket error occurred";
+    }
 }
