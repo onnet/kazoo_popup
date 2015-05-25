b779dd3e03f6b65095437710cfe7777efe41edce
Kirill Sysoev <kirill.sysoev@gmail.com>
2015-05-25 16:19:37 +0300

    update

--- caller.cpp
+++ caller.cpp
@@ -6,7 +6,6 @@ static const char * const kCallerInfoTemplate = "<html><body><table style='font-
                                           //      "<tr><td><font size=\"-1\">Caller dialed: </font></td><td><font size=\"+1\">%3</font></td></tr>"
                                                 "<tr><td><font size=\"-1\">Contact person: </font></td><td><font size=\"+1\">%4</font></td></tr>"
                                                 "<tr><td><font size=\"-1\">Login name: </font></td><td><a href=\"%10=%5\"><font size=\"+1\">%5</font></a></td></tr>"
-                                          //      "<tr><td><font size=\"-1\">Login name: </font></td><td><a href=\"https://onnet.su/login?login_name=%5\"><font size=\"+1\">%5</font></a></td></tr>"
                                                 "<tr><td><font size=\"-1\">Company: </font></td><td><font size=\"+1\">%7</font></td></tr>"
                                                 "<tr><td><font size=\"-1\">Balance: </font></td><td><font size=\"+1\">%8</font></td></tr>"
                                                 "<tr><td><font size=\"-1\">Email: </font></td><td><font size=\"+1\">%9</font></td></tr>"
--- defaults.h
+++ defaults.h
@@ -9,11 +9,11 @@ static const char * const kRealm = "your_account_name.sip.onnet.su";
 static const char * const kAuthUrl = "https://sip.onnet.su:8443";
 static const char * const kCrossbarUrl = "https://sip.onnet.su:8443";
 static const char * const kEventUrl = "https://sip.onnet.su:7777";
-static const char * const kInfoUrl = "https://localhost/kazoo_popup.php?caller_number={{Caller-ID-Number}}";
+static const char * const kInfoUrl = "https://onnet.su/popup_login?login_name";
 static const char * const kCrmUrl = "https://onnet.su/api/onnet/cidinfo?calling_number={{Caller-ID-Number}}";
 static const char * const kCrmHash = "vhbxzj947yisfbl";
-static const int kPopupTimeout = 30;
-static const int kCallDirection = 0;
+static const int kPopupTimeout = 50;
+static const int kCallDirection = 2;
 static const int kOpenUrl = 0;
 static const bool kRunAtStartup = true;
 
